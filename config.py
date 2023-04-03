67bfc360172db56b362203dc8b7ce3997741cfc0
slazarska <slazarska@protonmail.com>
2023-04-03 17:57:33 +0300

    changed configurations files, added poetry files

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
 import datetime
-import pydantic
 from datetime import date
-from appium.options.android import UiAutomator2Options
 from typing import Literal, Optional
+
+import pydantic
+from appium.options.android import UiAutomator2Options
+
 from spelly_mobile_test import utils
 
-EnvContext = Literal['local', 'browserstack']
+EnvContext = Literal['local', 'browserstack', 'personal']
 
 
 class Settings(pydantic.BaseSettings):
     # --- env by default ---
-    context: EnvContext = 'browserstack'
+    context: EnvContext = 'personal'
 
     # --- Appium Capabilities ---
     platformName: str = None
@@ -18,7 +20,6 @@ class Settings(pydantic.BaseSettings):
     deviceName: str = None
     app: Optional[str] = None
     appName: Optional[str] = None
-    # appWaitActivity: Optional[str] = None
     newCommandTimeout: Optional[int] = 60
 
     # --- > BrowserStack Capabilities ---
@@ -26,22 +27,21 @@ class Settings(pydantic.BaseSettings):
     buildName: Optional[str] = None
     browserstackSessionName: Optional[str] = None
     # --- > > BrowserStack credentials---
-    # browserstackUser: Optional[str] = None
-    # browserstackKey: Optional[str] = None
-    browserstackUser: Optional[str] = pydantic.Field(None, env='browserstack.browserstackUser')
-    browserstackKey: Optional[str] = pydantic.Field(None, env='browserstack.browserstackKey')
+    browserstackUser: Optional[str] = None
+    browserstackKey: Optional[str] = None
+    #browserstackUser: Optional[str] = pydantic.Field(None, env='browserstack.browserstackUser')
+    #browserstackKey: Optional[str] = pydantic.Field(None, env='browserstack.browserstackKey')
     udid: Optional[str] = None
 
     # --- Remote Driver ---
     remote_url: str = 'http://127.0.0.1:4723/wd/hub'  # default appium server url
 
     # --- Selene ---
-    timeout: float = 6.0
+    timeout: float = 8.0 # timeout: float = 6.0
 
     @property
     def run_on_browserstack(self):
-        return 'hub.browserstack.com' in self.remote_url
-        # return 'hub.browserstack.com/wd/hub' in self.remote_url
+        return 'hub.browserstack.com/wd/hub' in self.remote_url
 
     @property
     def driver_options(self):
@@ -52,18 +52,17 @@ class Settings(pydantic.BaseSettings):
             options.platform_name = self.platformName
         options.app = (
             utils.file.abs_path_from_project(self.app)
-            if self.app and (self.app.startswith('./') or self.app.startswith('../'))
+            if self.app.startswith('./') or self.app.startswith('../')
             else self.app
         )
         options.new_command_timeout = self.newCommandTimeout
         if self.udid:
             options.udid = self.udid
-        #if self.appWaitActivity:
-        #    options.app_wait_activity = self.appWaitActivity
         if self.run_on_browserstack:
             options.load_capabilities(
                 {
                     'platformVersion': self.platformVersion,
+                    # --- Browserstack Capabilities ---
                     'bstack:options': {
                         'projectName': self.projectName,
                         'buildName': f'{self.buildName}_{date.today()}',
@@ -78,10 +77,12 @@ class Settings(pydantic.BaseSettings):
 
     @classmethod
     def in_context(cls, env: Optional[EnvContext] = None) -> 'Settings':
+        """
+        factory method to init Settings with values from corresponding .env file
+        """
         asked_or_current = env or cls().context
         return cls(
-            #  _env_file=utils.file.abs_path_from_project(f'config.{asked_or_current}.env')
-            _env_file=utils.file.abs_path_from_project(f'config.{asked_or_current}config.local.env')
+            _env_file=utils.file.abs_path_from_project(f'config.{asked_or_current}.env')
         )
 
 

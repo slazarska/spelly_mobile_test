d749df53eed37824e28aaacb34e5183160ec6117
slazarska <slazarska@protonmail.com>
2023-04-03 18:31:46 +0300

    fixed app.py, added tests

--- spelly_mobile_test/model/app.py
+++ spelly_mobile_test/model/app.py
@@ -1,2 +1,5 @@
 from spelly_mobile_test.model.screens.main_screen import MainScreen
 from spelly_mobile_test.model.screens.spell_screen import SpellScreen
+
+main_screen = MainScreen()
+spell_screen = SpellScreen()
--- tests/test_spells.py
+++ tests/test_spells.py
@@ -1,18 +1,34 @@
 import allure
-from appium.webdriver.common.appiumby import AppiumBy
-from selene.support.conditions import have, be
-from selene.support.shared import browser
+from allure_commons.types import Severity
+from spelly_mobile_test.model import app
 
 
 @allure.tag("mobile")
-@allure.feature("Browserstack")
-@allure.story("Mobile tests on Wikipedia with Browserstack")
-def test_open_spell():
-    with allure.step('Click on the search field'):
-        browser.element((AppiumBy.ID, "header_text_view")).click()
-    with allure.step('Search "The Last of Us"'):
-        browser.element((AppiumBy.ID, "activity_selected_spell").should(be.visible))
-        # browser.element((AppiumBy.ACCESSIBILITY_ID, "spell_type")).should(be.visible))
-        # browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("The Last of Us")
-    #with allure.step('Check the search result is not null'):
-        #browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).should(have.size_greater_than(0))
+@allure.label('owner', 'slazarska')
+@allure.feature(' ')
+@allure.story(' ')
+class TestsSpells:
+    @allure.severity(Severity.BLOCKER)
+    @allure.title('Open spells screen')
+    def test_open_spell(self):
+        app.main_screen.open_spells_screen()
+        app.spell_screen.check_spells_page_is_opened()
+
+    @allure.severity(Severity.CRITICAL)
+    @allure.title(' ')
+    def test_go_back(self):
+        app.main_screen.open_spells_screen()
+        app.spell_screen.go_back_to_main_screen()
+
+    @allure.severity(Severity.NORMAL)
+    @allure.title('Check the content on spells screen')
+    def test_check_spell_name(self):
+        app.main_screen.open_spells_screen()
+        app.spell_screen.check_spells_name_on_the_spell_screen("Alohomora")
+
+    @allure.severity(Severity.NORMAL)
+    @allure.title('Check the content on spells screen')
+    def test_spell_has_action(self):
+        app.spell_screen.check_action_on_the_spell_screen()
+        app.spell_screen.check_spells_type_on_the_spell_screen()
+        app.spell_screen.check_quote_on_the_spell_screen()

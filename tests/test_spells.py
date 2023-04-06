faebcb5b057ff2a41b6628bc23c7f6ed15c95651
slazarska <slazarska@protonmail.com>
2023-04-06 11:46:13 +0300

    changed allure annotations

--- tests/test_spells.py
+++ tests/test_spells.py
@@ -6,10 +6,10 @@ from spelly_mobile_test.model import app
 @allure.tag('ui', 'mobile')
 @allure.label('owner', 'slazarska')
 @allure.feature('Spells')
-@allure.story('Elements on the spells page')
 class TestsSpells:
 
     @allure.severity(Severity.BLOCKER)
+    @allure.story('Main functionality')
     @allure.title('Open spells screen')
     def test_open_spell(self):
         app.main_screen.check_main_screen_is_opened()
@@ -17,6 +17,7 @@ class TestsSpells:
         app.spell_screen.check_spells_screen_is_opened()
 
     @allure.severity(Severity.CRITICAL)
+    @allure.story('Main functionality')
     @allure.title('Return to the main page from the spells page')
     def test_return_to_main_screen(self):
         app.main_screen.open_spells_screen()
@@ -24,12 +25,14 @@ class TestsSpells:
         app.main_screen.check_main_screen_is_opened()
 
     @allure.severity(Severity.NORMAL)
+    @allure.story('Elements on the spells page')
     @allure.title('Check the spells name')
     def test_spells_name(self):
         app.main_screen.open_spells_screen()
         app.spell_screen.check_spells_name('Alohomora')
 
     @allure.severity(Severity.NORMAL)
+    @allure.story('Elements on the spells page')
     @allure.title('Check the main attributes of spell on the spells page')
     def test_spells_description(self):
         app.main_screen.open_spells_screen()
@@ -38,6 +41,7 @@ class TestsSpells:
         app.spell_screen.check_spells_quote()
 
     @allure.severity(Severity.NORMAL)
+    @allure.story('Favorites')
     @allure.title('Check the spell can be added to the list of favorite spells')
     def test_add_spell_to_favorites(self):
         app.main_screen.tap_favorite_icon()
